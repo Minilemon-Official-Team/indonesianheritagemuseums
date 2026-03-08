25bfb25b6eabf57c48e6d02ac76fde739241a961
GhazaRiZ <74690464+LevianthProject@users.noreply.github.com>
2026-03-08 07:20:43 +0700

    update map

--- src/app/pages/Visit.tsx
+++ src/app/pages/Visit.tsx
@@ -144,8 +144,17 @@ export default function Visit() {
         {/* Map */}
         <div className="bg-white rounded-lg shadow-lg p-8 mb-12">
           <h2 className="font-['Cinzel'] text-2xl text-[#2B2B2B] mb-6 text-center">Find Us</h2>
-          <div className="aspect-video bg-[#E7DED0] rounded flex items-center justify-center">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4924.694188352999!2d112.52393289999999!3d-7.8840109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78872dd3c46115%3A0x1d9644037c54d349!2sIndonesian%20Heritage%20Museum!5e1!3m2!1sen!2sid!4v1772927997715!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <div className="aspect-video rounded overflow-hidden">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4924.694188352999!2d112.52393289999999!3d-7.8840109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78872dd3c46115%3A0x1d9644037c54d349!2sIndonesian%20Heritage%20Museum!5e1!3m2!1sen!2sid!4v1772927997715!5m2!1sen!2sid"
+              className="w-full h-full border-0"
+              width="100%"
+              height="450"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
           </div>
         </div>
 
